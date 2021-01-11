25adc0366a4698950d1bce009f2a631a59fda4bb
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2021-01-11 15:58:17 -0800

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Johnson Hsiung on 1/11/21.
+//  Copyright © 2021 Johnson Hsiung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
